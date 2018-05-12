48428d2326cc7b4a8ba2308e657c3d6e8c9c2cab
wangbo <240868722@qq.com>
2018-05-12 10:52:40 +0800

    1.2

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,5 +1,8 @@
 <template>
-	<h1>home</h1>
+	<div>
+	<h1>hello wangbo</h1>
+	
+	</div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from "../pages/home/Home"
-import List from "../pages/list/List"
+
 
 Vue.use(Router)
 
 export default new Router({
-	mode:"history",
+  mode:"history",
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/list',
-      name: 'Home',
-      component: List
     }
+  
   ]
 })
